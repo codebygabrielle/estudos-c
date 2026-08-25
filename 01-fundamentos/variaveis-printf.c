f9843ff73f1732c80b0fc0e718142cbbdeee4aae
Gabrielle <gabrielleresende55@gmail.com>
2026-08-24 22:50:30 -0300

    Add basic C program for variable demonstration

---
+++ 01-fundamentos/variaveis-printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h> //importacao da biblioteca
+
+
+int main()
+
+
+{
+	
+	int numero1 = 10;
+	float numero2 = 50.5;
+	double numero3 = 50.2356;
+	char nome[10] = "Gabi";
+	
+	printf("Ola, meu primeiro codigo c");
+	printf("\nNome: %s",nome);
+	printf("\nOs numeros sao: %d, %.2f, %.10lf, ok", numero1, numero2, numero3);
+	
+return 0;	
+	
+}
